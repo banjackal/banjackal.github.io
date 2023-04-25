bb0455964ed119c6670c41a6a74f6c1c6ff58611
Nick Raeon <nick.raeon@gmail.com>
2023-04-25 08:26:05 -0400

    move rust lambda up a level

--- python/print_env.py
+++ python/print_env.py
@@ -1,8 +1,19 @@
 import os
+from typing import Optional
+
 def getEnv():
-    foo = os.environ['foo']
+    foo = os.environ.get('foo','foobar')
     return foo
+
+
+def dontReturn() -> Optional[str]:
+    print("not returning anything")
 
 if __name__ == '__main__':
     foo = getEnv()
     print(foo)
+    if foo is not None:
+        foo = dontReturn()
+    print(foo)
+
+    
